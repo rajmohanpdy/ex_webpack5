8bdc57cea7baa8f9ff8a17daf942cca207225769
rajmohanpdy <raj4057@gmail.com>
2021-01-04 12:31:34 +0530

    latest commit jan-4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-const $ = require('jquery');
+import $ from 'jquery';
+import _ from 'lodash';
+import './style.css';
+import Icon from './rajmohan.jpg';
 
 $('body').css({ 'background-color': '#eeeeee' });
 $('h1#title')
   .css({ 'font-size': '80px', 'text-align': 'center', 'margin-top': '100px' })
-  .text('Hello from webpack-dev-server');
+  .addClass('hello')
+  .text('Hello from webpack-dev-server sdsdsd');
+
+  // function component() {
+  //   const element = document.createElement('div');
+  //    // Lodash, now imported by this script
+  //  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  //  element.classList.add('hello');
+ 
+  //  // Add the image to our existing div.
+  //  const myIcon = new Image();
+  //  myIcon.src = Icon;
+ 
+  //  element.appendChild(myIcon);
+ 
+  //   return element;
+  // }
+  
+  // document.body.appendChild(component());
+
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
